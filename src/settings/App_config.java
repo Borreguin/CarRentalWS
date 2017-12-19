204523e3696ec2f6e395fd787f47bed8f7ba1467
Roberto S <borregosrober77@hotmail.com>
2017-12-19 18:05:24 +0100

    Initial Setting. Settling down the software framework.

---
+++ src/settings/App_config.java
@@ -0,0 +1,18 @@
+package settings;
+
+/***
+ *   Author: Roberto Sanchez A. 
+ *   Date:   12/19/17
+ ***/
+public class App_config {
+    /***
+     * Contains global variables and settings about this application
+     * this assists the installation of the CarRentalWS application in other environments.
+     */
+
+
+    public final static String BusinessModelDB_IP = "localhost";
+    public final static String BusinessModelDB_Port = "9090";
+    public final static String BusinessTransactionsDB_IP = "localhost";
+    public final static String BusinessTransactionsDB_Port = "9090";
+}
---
+++ src/settings/Business_conf.java
@@ -0,0 +1,14 @@
+package settings;
+
+/***
+ *   Author: Roberto Sanchez A. 
+ *   Date:   12/19/17
+ ***/
+public class Business_conf {
+
+    /**
+     * Contains variables and settings of the business logic
+     */
+    public static final int young_age = 25;
+
+}
